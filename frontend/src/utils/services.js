de458c22647283c59805de51cb5f62453948376c
Muhammad Haroon <theharoonsocial@gmail.com>
2025-02-05 21:17:05 +0500

    set api url

--- frontend/src/utils/services.js
+++ frontend/src/utils/services.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://topnlink-api.vercel.app/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
